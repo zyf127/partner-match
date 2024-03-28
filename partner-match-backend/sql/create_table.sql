a53855620bee5522a47c4c7f4fa136800c7e4a7e
zyf <zyf19863@163.com>
2024-03-28 17:04:43 +0800

    数据库表设计

---
+++ partner-match-backend/sql/create_table.sql
@@ -0,0 +1,42 @@
+-- auto-generated definition
+create table tag
+(
+    id          bigint auto_increment comment 'id'
+        primary key,
+    tag_name    varchar(256)                       null comment '标签名称',
+    user_id     bigint                             null comment '用户 id',
+    parent_id   bigint                             null comment '父标签 id',
+    is_parent   tinyint                            null comment '0 - 不是，1 - 父标签',
+    create_time datetime default CURRENT_TIMESTAMP null comment '创建时间',
+    update_time datetime default CURRENT_TIMESTAMP null on update CURRENT_TIMESTAMP comment '更新时间',
+    is_delete   tinyint  default 0                 not null comment '是否删除',
+    constraint unique_index_tag_name
+        unique (tag_name) comment '基于 tag_name 创建的唯一索引'
+)
+    comment '标签';
+
+create index index_user_id
+    on tag (user_id)
+    comment '基于 user_id 创建的索引';
+
+
+-- auto-generated definition
+create table user
+(
+    id            bigint auto_increment comment 'id'
+        primary key,
+    username      varchar(256)                       null comment '用户昵称',
+    user_account  varchar(256)                       null comment '账号',
+    avatar_url    varchar(1024)                      null comment '用户头像',
+    gender        tinyint                            null comment '性别',
+    user_password varchar(512)                       not null comment '密码',
+    phone         varchar(128)                       null comment '电话',
+    email         varchar(512)                       null comment '邮箱',
+    user_status   int      default 0                 not null comment '状态 0 - 正常',
+    user_role     tinyint  default 0                 not null comment '角色 0 - 普通用户 1 - 管理员',
+    tags          varchar(1024)                      null comment '标签列表',
+    create_time   datetime default CURRENT_TIMESTAMP null comment '创建时间',
+    update_time   datetime default CURRENT_TIMESTAMP null on update CURRENT_TIMESTAMP comment '更新时间',
+    is_delete     tinyint  default 0                 not null comment '是否删除'
+)
+    comment '用户';
