e9a6502a542fe59e4d3ac47a6a1c5db8cb31d5c7
zyf <zyf19863@163.com>
2024-03-28 16:19:56 +0800

    后端项目初始化

---
+++ partner-match-backend/src/main/java/com/zyf/PartnerMatchBackendApplication.java
@@ -0,0 +1,13 @@
+package com.zyf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PartnerMatchBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PartnerMatchBackendApplication.class, args);
+    }
+
+}
---
+++ partner-match-backend/src/test/java/com/zyf/PartnerMatchBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zyf;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PartnerMatchBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
