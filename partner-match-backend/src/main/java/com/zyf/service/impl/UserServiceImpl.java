bcd346bbf727f87d935ef27001689fc5069be5af
zyf <zyf19863@163.com>
2024-04-01 21:33:46 +0800

    单机登录改为分布式 session 登录

--- partner-match-backend/src/main/java/com/zyf/service/impl/UserServiceImpl.java
+++ partner-match-backend/src/main/java/com/zyf/service/impl/UserServiceImpl.java
@@ -145,7 +145,7 @@ public class UserServiceImpl extends ServiceImpl<UserMapper, User>
      */
     @Override
     public int userLogout(HttpServletRequest request) {
-        request.removeAttribute(USER_LOGIN_STATE);
+        request.getSession().removeAttribute(USER_LOGIN_STATE);
         return 1;
     }
 
